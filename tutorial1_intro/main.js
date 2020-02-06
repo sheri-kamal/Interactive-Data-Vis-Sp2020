e465565b8a5691088365e0e07f71e889f75c4795
sheri-kamal <60554283+sheri-kamal@users.noreply.github.com>
2020-02-06 14:59:48 -0500

    Tutorial 1

--- tutorial1_intro/main.js
+++ tutorial1_intro/main.js
@@ -1,32 +1,44 @@
-d3.csv("../../tutorial1_intro/311 Data - Water Quality.csv").then(data => {
-    console.log("data", data);
+d3.csv("../../tutorial1_intro/311 Data - Water Quality and Illness.csv").then(data => {
+  console.log("data", data);
   
-    const table = d3.select("#d3-table");
+  const table = d3.select("#d3-table");
   
-    const thead = table.append("thead");
-    thead
-      .append("tr")
-      .append("th")
-      .attr("colspan", "7")
-      .text("Water Quality Complaints in NYC");
+  const thead = table.append("thead");
+  thead
+    .append("tr")
+    .append("th")
+    .attr("colspan", data.columns.length)
+    .text("Water Quality and Waterborne Illness Complaints in NYC");
   
-    thead
-      .append("tr")
-      .selectAll("th")
-      .data(data.columns)
-      .join("td")
-      .text(d => d);
+  thead
+    .append("tr")
+    .selectAll("th")
+    .data(data.columns)
+    .join("th")
+    .text(d => d);
   
-    const rows = table
-      .append("tbody")
-      .selectAll("tr")
-      .data(data)
-      .join("tr");
+  const rows = table
+    .append("tbody")
+    .selectAll("tr")
+    .data(data)
+    .join("tr");
   
-    rows
-      .selectAll("td")
-      .data(d => Object.values(d))
-      .join("td")
-      .attr("class", d => d > 1762 ? 'above-average' : null)
-      .text(d => d);
-  });
+  rows
+    .selectAll("td")
+    .data(d => Object.values(d))
+    .join("td")
+    .attr("class", (d, i) => {
+      if((i = 2 && d > 21)||(i = 3 && d > 1762)) {
+        return "above-average"
+      }
+    })
+    .text(d => d);
+});
+
+d3.select("body")
+  .append("div")
+  .attr("class","source")
+d3.select(".source")
+  .append("a")
+  .attr("href", "https://data.cityofnewyork.us/Social-Services/311-Service-Requests-from-2010-to-Present/erm2-nwe9/data")
+  .text("Source: 311 Service Requests from 2010 to Present")
