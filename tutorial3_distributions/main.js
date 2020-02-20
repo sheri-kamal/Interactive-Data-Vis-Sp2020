4e54a1051349ad57e72417df99f16d06a28f74a7
sheri-kamal <60554283+sheri-kamal@users.noreply.github.com>
2020-02-20 15:21:15 -0500

    Tutorial 3

--- tutorial3_distributions/main.js
+++ tutorial3_distributions/main.js
@@ -10,15 +10,15 @@ let svg;
 let xScale;
 let yScale;
 
+
 /* APPLICATION STATE */
 let state = {
   data: [],
-  selection: "All" // + YOUR FILTER SELECTION
+  selectedborough: "All"
 };
 
 /* LOAD DATA */
-d3.json("../tutorial3_distributions/311 Data - Water Quality and Illness.json", d3.autoType).then(raw_data => {
-  // + SET YOUR DATA PATH
+d3.csv("../tutorial3_distributions/311 Data - Water Quality and Illness.csv", d3.autoType).then(raw_data => {
   console.log("raw_data", raw_data);
   state.data = raw_data;
   init();
@@ -38,28 +38,23 @@ function init() {
     .domain(d3.extent(state.data, d => d["Waterborne Illness"]))
     .range([height - margin.bottom, margin.top]);
 
-  const colorScale = d3
-    .scaleOrdinal(d3.schemeTableau10)
-    .domain(data.map(d => d.Borough));
   // + AXES
   const xAxis = d3.axisBottom(xScale);
   const yAxis = d3.axisLeft(yScale);
+
   // + UI ELEMENT SETUP
-
   const selectElement = d3.select("#dropdown").on("change", function() {
+    console.log("new selected borough is", this.value);
     // `this` === the selectElement
-    // 'this.value' holds the dropdown value a user just selected
-
-    state.selection = this.value
-    console.log("new borough is", this.value);
-    state.selection = this.value;
+    // this.value holds the dropdown value a user just selected
+    state.selectedborough = this.value;
     draw(); // re-draw the graph based on this new selection
   });
 
   // add in dropdown options from the unique values in the data
   selectElement
     .selectAll("option")
-    .data(["All", "Bronx", "Brooklyn", "Manhattan", "Queens", "Staten Island"]) // + ADD UNIQUE VALUES
+    .data(["All", "Bronx", "Brooklyn", "Queens", "Manhattan", "Staten Island"]) // unique data values-- (hint: to do this programmatically take a look `Sets`)
     .join("option")
     .attr("value", d => d)
     .text(d => d);
@@ -70,6 +65,7 @@ function init() {
     .append("svg")
     .attr("width", width)
     .attr("height", height);
+
   // + CALL AXES
   svg
     .append("g")
@@ -78,11 +74,11 @@ function init() {
     .call(xAxis)
     .append("text")
     .attr("class", "axis-label")
-    .attr("x", "50%")
+    .attr("Queens", "50%")
     .attr("dy", "3em")
     .text("Water Quality");
-  
-    svg
+
+  svg
     .append("g")
     .attr("class", "axis y-axis")
     .attr("transform", `translate(${margin.left},0)`)
@@ -93,53 +89,62 @@ function init() {
     .attr("dx", "-3em")
     .attr("writing-mode", "vertical-rl")
     .text("Waterborne Illness");
+
   draw(); // calls the draw function
 }
+
 
 /* DRAW FUNCTION */
  // we call this everytime there is an update to the data/state
 function draw() {
   let filteredData = state.data;
   // + FILTER DATA BASED ON STATE
-  if (state.selection !== "All") {
-    filteredData = state.data.filter(d => d.Borough === state.selection);
+  if (state.selectedborough
+ !== "All") {
+    filteredData = state.data.filter(d => d.Borough === state.selectedborough);
   }
+
   const dot = svg
-   .selectAll("circle")
-   .data(filteredData, d => d.name)
-   .join(
-     enter => 
-      enter
-      .append("circle")
-      .attr("class", "dot") 
-      .attr("stroke", "lightgrey")
-      .attr("opacity", 0.5)
-      .attr("fill", d => colorScale(d.Borough))
-      .attr("r", radius)
+    .selectAll(".dot")
+    .data(filteredData, d => d.Borough)
+    .join(
+      enter =>
+        enter
+          .append("circle")
+          .attr("class", "dot") 
+          .attr("stroke", "lightgrey")
+          .attr("opacity", 0.5)
+          .attr("fill", d => {
+            if (d.Borough === "Bronx") return "blue";
+            else if (d.Borough === "Brooklyn") return "green";
+            else if (d.Borough === "Queens") return "orange";
+            else if (d.Borough === "Manhattan") return "red"; 
+            else return "purple";
+          })
+          .attr("r", radius)
           .attr("cy", d => yScale(d["Waterborne Illness"]))
           .attr("cx", d => margin.left) // initial value - to be transitioned
           .call(enter =>
-            enter
-              .transition() // initialize transition
-              .delay(d => 500 * d["Water Quality"]) // delay on each element
-              .duration(500) // duration 500ms
-              .attr("cx", d => xScale(d["Water Quality"]))), // + HANDLE ENTER SELECTION
-     update => 
-      update.call(update =>
-        update
-        .transition()
-        .duration(250)
-        .attr("stroke", "black")
-        .transition()
-        .duration(250)
-        .attr("stroke", "lightgrey")), // + HANDLE UPDATE SELECTION
-     exit => 
-      exit.call(exit =>
-        exit
-        .transition()
-        .delay(d => 50 * d["Water Quality"])
-        .duration(500)
-        .attr("cx", width)
-        .remove()) // + HANDLE EXIT SELECTION
-  );
+              enter
+                .attr("cx", d => xScale(d["Water Quality"])) // + HANDLE ENTER SELECTION
+                .transition()
+                .delay(d => 500 * d["Water Quality"]) // delay on each element    
+            ),
+        update =>
+          update.call(update =>
+            update
+              .transition()
+              .duration(250)
+              .attr("stroke", "black")
+              .transition()
+              .duration(250)
+              .attr("stroke", "lightgrey") // + HANDLE UPDATE SELECTION
+          ),
+        exit =>
+          exit.call(exit =>
+            exit
+              .attr("cx", width)
+              .remove() // + HANDLE EXIT SELECTION
+        )
+    );
 }
